521cd9b7dae7f5d26b7da15677a374839dfe0107
Jaqueline Rabelo <125192947+JaquelineRabelo@users.noreply.github.com>
2025-01-29 14:07:45 +0000

    new

---
+++ modulo7/Exercicios/exc1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Modulo7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exc1.Run(); // Chama o método Run da classe Exc1
+        }
+    }
+}
---
+++ modulo7/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Exercicios; // Certifique-se de que isso corresponde ao namespace no exc1.cs
+
+namespace Modulo7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            exc1.Run(); // Chama o método Run da classe Exc1
+        }
+    }
+}
